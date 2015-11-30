421a287a97e367c4048857608ed9a1275fc0c956
Yanke Guo <guoyk@1mxian.com>
2015-12-01 01:52:27 +0800

    add Gemfile; add GenR

---
+++ GenR.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/env ruby
+
+require 'thor'
+require 'fileutils'
+require 'pathname'
+
+class GenR < Thor
+  IMAGE_FILE_EXTS = ['png', 'jpg', 'jpeg', 'tiff']
+
+  desc "create", "Create a resource file R."
+  method_option :directory, aliases: ['-d'], desc: 'Resource directory', required: true, type: :string
+  method_option :target,    aliases: ['-t'], desc: 'Target file, will append .h, .m for Obj-C', required: true, type: :string
+  def create
+    directory = File.expand_path options[:directory], Dir.pwd
+    target    = File.expand_path options[:target],    Dir.pwd
+    # Get all xcassets
+    Dir["#{directory}/**/*.xcassets"].each do |asset_folder|
+      asset_path = Pathname.new asset_folder
+      Dir["#{asset_folder}/**/*.imageset"].each do |image_folder|
+        raise "#{image_folder} is not a directory" unless File.directory? image_folder
+        full_query_path     = File.expand_path "../#{File.basename(image_folder, ".*")}", image_folder
+        relative_query_path = Pathname.new(full_query_path).relative_path_from(asset_path).to_s
+      end
+    end
+  end
+
+end
+
+GenR.start
